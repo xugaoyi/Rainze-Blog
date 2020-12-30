3ffdaff2cc62d8fa9b78a25ddb411b667773e240
liyuze <ezuylee@gmail.com>
2020-12-30 23:08:05 +0800

    发行版

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# deploy to github
+#echo 'b.ezuy-lee.github.io' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:Ezuy-Lee/blog.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://Ezuy-Lee:${GITHUB_TOKEN}@github.com/Ezuy-Lee/blog.git
+  git config --global user.name "ezuylee"
+  git config --global user.email "ezuylee@gmail.com"
+fi
+git init
+git add -A
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
+
+# deploy to coding
+# echo 'blog.liyuze.work\liyuze.work' > CNAME  # 自定义域名
+# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
+
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+ # codingUrl=git@e.coding.net:ezuylee/blog/blog.git
+#else
+ # codingUrl=https://e.coding.net/ezuylee/blog/blog.git
+#fi
+#git add -A
+#git commit -m "${msg}"
+#git push -f $codingUrl master # 推送到coding
+
+cd - # 退回开始所在目录
+rm -rf docs/.vuepress/dist
---
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -0,0 +1,155 @@
+// nav
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '后端',
+    link: '/back_end/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+      {
+        text: 'Java基础篇',
+        link: '/Java基础篇/',
+       
+      },
+      {
+        text: 'Java框架篇',
+        link: '/back_end/Java框架篇/',
+      },
+      {
+        text: 'CVS版本控制',
+        link: '/CVS版本控制/',
+        items: []
+      },
+      {
+        text: '数据库和缓存',
+        link: '/pages/bd16e5/',
+        items: [
+          {
+            text: '关系型数据库',
+           
+          },
+          {
+            text: 'NoSQL数据库',
+           
+          },
+          {
+            text: '更多数据库',
+           
+          },
+        ]
+      },
+      {
+        text: '设计模式和架构',
+       
+      },
+      {
+        text: '容器化和中间件',
+       
+      },
+      {
+        text: 'Linux运维和测试',
+       
+      },
+      {
+        text: '大数据和云计算',
+       
+      },
+      {
+        text: '工具和环境',
+       
+      },
+    ],
+  },
+  {
+    text: '前端',
+    link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+      {
+        text: '前端基础',
+        
+      },
+      {
+        text: '前端框架',
+      
+      },
+      {
+        text: '可视化',
+       
+      },
+      {
+        text: 'UI设计和产品',
+       
+      },
+    ],
+  },
+  {
+    text: '工作',
+    link: '/work/',
+    items: [
+      { text: '个人', link: '/pages/9a7ee40fc232253e/' },
+      { text: '公司', link: '/pages/4c778760be26d8b3/' },
+    
+    ],
+  },
+  {
+    text: '工具箱',
+    link: '/tool/',
+    items: [
+      {
+        text: '在线工具',
+       items:[
+        { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+       ]
+      },
+      {
+        text: '后端工具',
+       
+      },
+      {
+        text: '前端工具',
+      
+      },
+    ],
+  },
+  {
+    text: '导航',
+    link: '/navigation/',
+    items: [
+      {
+        text: '后端学习',
+       
+      },
+      {
+        text: '前端学习',
+       
+      },
+      {
+        text: '软件资源',
+        
+      },
+      
+    ],
+  },
+  {
+    text: '更多',
+    link: '/more/',
+    items: [
+      { text: '学习阅读', link: '/pages/f2a556/' },
+      { text: '面试经验', link: '/pages/aea6571b7a8bae86/' },
+      { text: '心情杂谈', link: '/pages/2d615df9a36a98ed/' },
+      { text: '友情链接', link: '/friends/' },
+    ],
+  },
+  
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' },
+    ],
+  },
+  { text: '关于', link: '/about/' },
+]
